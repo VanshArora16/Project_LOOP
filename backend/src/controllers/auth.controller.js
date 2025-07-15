b643c5b06e8d20e5cc881660a16e280d5ff53550
VanshArora16 <vansh4047@gmail.com>
2025-07-15 10:25:06 +0530

    Initial commit with project files

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("<h1>signup route</h1>");
+};
+
+export const login = (req, res) => {
+    res.send("<h1>login route</h1>");
+};
+
+export const logout = (req, res) => {
+    res.send("<h1>logout route</h1>");
+};
---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use("/api/auth/", authRoutes);
+
+app.listen(PORT, () => {console.log(`server is running on ${PORT}`);});
