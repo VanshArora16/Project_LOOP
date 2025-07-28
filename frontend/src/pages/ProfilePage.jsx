b0ec6af4bf0cee666acd4eeeaceee9c1367ff90e
VanshArora16 <vansh4047@gmail.com>
2025-07-28 11:27:14 +0530

    corrected profilePage

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -100,7 +100,7 @@ const ProfilePage = () => {
                         <div className="space-y-3 text-sm">
                             <div className="flex items-center justify-between py-2 border-b border-zinc-700">
                                 <span>Member Since</span>
-                                <span>{authUser.createdAt?.split("T")[0]}</span>
+                                <span className="text-base-content">{authUser.createdAt?.split("T")[0]}</span>
                             </div>
                             {/* account status */}
                             <div className="flex items-center justify-between py-2">
