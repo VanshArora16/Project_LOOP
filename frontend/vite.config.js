cf878501e902bd63d0f8061e9b140138669c912f
VanshArora16 <vansh4047@gmail.com>
2025-08-07 23:15:55 +0530

    updated vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     optimizeDeps: {
-        include: ["lucide-react"],
+        include: ["lucide-react", "zustand", "react-router-dom","socket.io-client","axios"],
     },
-    base: './', 
+    base: "./",
 });
